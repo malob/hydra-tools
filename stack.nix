ccbe8ec5dfabd3dbc48e6e99ab1acbdae17e0b8f
Malo Bourgon <mbourgon@gmail.com>
2023-04-29 14:49:02 -0700

    Update dependencies

--- stack.nix
+++ stack.nix
@@ -1,6 +1,6 @@
 let pkgs = import (import ./default.nix).inputs.nixpkgs { }; in
 pkgs.haskell.lib.buildStackProject {
-  ghc = pkgs.haskell.compiler.ghc902;
+  ghc = pkgs.haskell.compiler.ghc927;
   name = "hydra-tools";
   buildInputs = [ pkgs.zlib ];
 }
